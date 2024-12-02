a44615144e01acffc3ff709be65e8c77046c8f7a
Mohammed Abderrahmane <louriachiabderrahman13@gmail.com>
2024-12-02 15:26:28 +0100

    ➕ added home diractory error handling

--- helpers/config.js
+++ helpers/config.js
@@ -1,6 +1,18 @@
 import dotenv from "dotenv";
+import FileSystem from "fs";
+import { exit } from "process";
+import { logger } from "./logger.js";
 
 dotenv.config();
 const env = { ...process.env };
+
+try {
+  logger.info("server started");
+  if (!FileSystem.existsSync(env.BLOGS_FOLDER))
+    FileSystem.mkdirSync(env.BLOGS_FOLDER, { recursive: true });
+} catch (error) {
+    logger.error(error);
+    setTimeout(() => exit(1), 1000);
+}
 
 export default env;
