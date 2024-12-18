bff45957c0363fee6bd6f0a08c2f84b79d276433
Mohammed Abderrahmane <louriachiabderrahman13@gmail.com>
2024-12-18 21:28:27 +0100

    ➕ addes style to blog page

--- ui-client/src/components/Blog/index.jsx
+++ ui-client/src/components/Blog/index.jsx
@@ -5,13 +5,6 @@ function Blog({ blog }) {
     <div className="blog-page">
       <h1>{blog.title}</h1>
       <div className="blog-content" innerHTML={blog.content} />
-
-      {blog.additionContent && (
-        <>
-          <h1>{blog.additionTitle}</h1>
-          <div className="blog-content" innerHTML={blog.additionContent} />
-        </>
-      )}
     </div>
   );
 }
