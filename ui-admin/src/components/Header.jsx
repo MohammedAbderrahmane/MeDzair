e74c08015ca7ac4170c271cf86d1c2588e652524
Mohammed Abderrahmane <louriachiabderrahman13@gmail.com>
2024-12-23 11:33:46 +0100

    ➕ Add welcome message and logout option

--- ui-admin/src/components/Header.jsx
+++ ui-admin/src/components/Header.jsx
@@ -1,10 +1,19 @@
-import { For } from "solid-js";
+import { For, Show, useContext } from "solid-js";
+
+import UserContext from "../reusable_components/Context/user.jsx";
+import AuthService from "../services/auth.js";
 
 function Header({ navItems }) {
+  const [user, setUser] = useContext(UserContext);
+
   const links = navItems || [
     { name: "home", href: "/" },
     { name: "new blog", href: "/blogs/new" },
   ];
+  const handleLog = () => {
+    AuthService.disconnect(setUser);
+    window.location.href = "/auth";
+  };
 
   return (
     <header style={{ border: "solid" }}>
@@ -12,7 +21,9 @@ function Header({ navItems }) {
         <h1 className="tiny5-regular">
           <a href="/">~/admin.medzair.com</a>
         </h1>
-        <span>Welcome Mohammed to the Dashboard</span>
+        <span>
+          Welcome <b>{user.username}</b> to the Dashboard
+        </span>
       </div>
       <nav>
         <ul>
@@ -23,6 +34,13 @@ function Header({ navItems }) {
               </li>
             )}
           </For>
+          <Show when={user}>
+            <li>
+              <button class="btn-ancher" onClick={handleLog}>
+                logout
+              </button>
+            </li>
+          </Show>
         </ul>
       </nav>
     </header>
