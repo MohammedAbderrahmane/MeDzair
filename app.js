2e5504697c18fb96a3be0655c2ef3388fea34aa6
Mohammed Abderrahmane <louriachiabderrahman13@gmail.com>
2024-12-23 12:04:43 +0100

    ✔️ Fix issue with rendring pages

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import "express-async-errors";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 import {
   generalErrorHandler,
@@ -10,17 +12,21 @@ import blogRouter from "./controllers/blog.js";
 import { loggerMiddleware } from "./helpers/logger.js";
 import authRouter from "./controllers/auth.js";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(loggerMiddleware);
 
-app.use(express.static("dist"));
 app.use("/api/blogs", blogRouter);
 app.use("/api/auth", authRouter);
-// app.use("/api");
-// app.use("/api");
+
+app.use(express.static(path.join(__dirname, "dist-ui-client")));
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "dist-ui-client/index.html"));
+});
 
 app.use(specificErrorHandler);
 app.use(generalErrorHandler);
