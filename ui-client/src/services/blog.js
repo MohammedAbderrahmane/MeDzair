63c2dd4d30b6d321a477fdf766eb34c6f89b4eb2
Mohammed Abderrahmane <louriachiabderrahman13@gmail.com>
2025-01-30 13:20:27 +0100

    sdqsqddqs

--- ui-client/src/services/blog.js
+++ ui-client/src/services/blog.js
@@ -3,9 +3,18 @@ import axios from "axios";
 const baseUrl = "/api/blogs";
 
 const getAll = async () => {
+  const formatDateString = (date) => date.split("/").reverse().join("-");
+
   try {
     const response = await axios.get(baseUrl);
-    return response.data;
+    const blogs = response.data;
+    blogs.sort(
+      (a, b) =>
+        Date.parse(formatDateString(b.date)) -
+        Date.parse(formatDateString(a.date))
+    );
+
+    return blogs;
   } catch (error) {
     const newError = new Error(error.response?.data?.error || "Network error");
     newError.status = error.response.status;
